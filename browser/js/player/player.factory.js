8274f9420cabca57669f76699b765991fb200ed0
Sean Colandrea <Sean@Seans-MacBook-Pro.local>
2016-02-09 17:39:30 -0500

    Finish factory specs

--- browser/js/player/player.factory.js
+++ browser/js/player/player.factory.js
@@ -2,4 +2,66 @@
 
 juke.factory('PlayerFactory', function(){
   // non-UI logic in here
+  var audio = document.createElement('audio');
+  var playing = false;
+  var currentSong = null;
+  var songListGlobal = null;
+  var currentIndex;
+
+  return {
+  	start: function(song, songList) {
+  		if (this.isPlaying()) {
+  			this.pause();
+  		}
+
+  		if (songList) {
+  			songListGlobal = songList;
+  			currentIndex = songList.indexOf(song);
+  		}
+
+  		audio.src = song.audioUrl;
+  		audio.load();
+  		audio.play();
+
+  		currentSong = song;
+  		playing = true;
+  	},
+  	pause: function() {
+  		audio.pause();
+  		playing = false;
+  	},
+  	resume: function() {
+  		audio.play();
+  		playing = true;
+  	},
+  	isPlaying: function() {
+  		return playing;
+  	},
+  	getCurrentSong: function() {
+  		return currentSong;
+  	},
+  	next: function() {
+  		if (currentIndex === songListGlobal.length - 1) {
+  			currentIndex = 0;
+  		} else {
+  			currentIndex++;
+  		}
+
+  		this.start(songListGlobal[currentIndex])
+  	},
+  	previous: function() {
+		if (currentIndex === 0) {
+  			currentIndex = songListGlobal.length - 1;
+  		} else {
+  			currentIndex--;
+  		}
+
+  		this.start(songListGlobal[currentIndex])
+  	},
+  	getProgress: function() {
+  		if (!playing) return 0;
+
+  		return audio.currentTime / audio.duration;
+  	}
+  }
 });
--- browser/js/player/player.factory.spec.js
+++ browser/js/player/player.factory.spec.js
@@ -16,6 +16,10 @@ describe('`PlayerFactory` factory', function () {
       return elem;
     };
     // test song sources
+  });
+
+  before(function () {
+    // test song sources
     song1 = { audioUrl: 'http://www.stephaniequinn.com/Music/Allegro%20from%20Duet%20in%20C%20Major.mp3' };
     song2 = { audioUrl: 'http://www.stephaniequinn.com/Music/Jazz%20Rag%20Ensemble%20-%2010.mp3' };
   });
@@ -53,7 +57,7 @@ describe('`PlayerFactory` factory', function () {
 
   });
 
-  xdescribe('#pause', function () {
+  describe('#pause', function () {
 
     it("calls audio's `pause`", function () {
       chai.spy.on(HTMLAudioElement.prototype, 'pause');
@@ -63,7 +67,7 @@ describe('`PlayerFactory` factory', function () {
 
   });
 
-  xdescribe('#resume', function () {
+  describe('#resume', function () {
 
     it("calls audio's `play`", function () {
       chai.spy.on(HTMLAudioElement.prototype, 'play');
@@ -73,7 +77,7 @@ describe('`PlayerFactory` factory', function () {
 
   });
 
-  xdescribe('#isPlaying', function () {
+  describe('#isPlaying', function () {
 
     it('returns false when song is not playing', function () {
       expect(PlayerFactory.isPlaying()).to.equal(false);
@@ -95,7 +99,7 @@ describe('`PlayerFactory` factory', function () {
 
   });
 
-  xdescribe('#getCurrentSong', function () {
+  describe('#getCurrentSong', function () {
 
     it('defaults to null', function () {
       expect(PlayerFactory.getCurrentSong()).to.equal(null);
@@ -114,7 +118,7 @@ describe('`PlayerFactory` factory', function () {
 
   });
 
-  xdescribe('#next', function () {
+  describe('#next', function () {
 
     var songList;
 
@@ -139,7 +143,7 @@ describe('`PlayerFactory` factory', function () {
 
   });
 
-  xdescribe('#previous', function () {
+  describe('#previous', function () {
 
     var songList;
 
@@ -165,7 +169,7 @@ describe('`PlayerFactory` factory', function () {
   });
 
 
-  xdescribe('#getProgress', function () {
+  describe('#getProgress', function () {
 
     it('is 0 before playing song', function () {
       expect(PlayerFactory.getProgress()).to.equal(0);
